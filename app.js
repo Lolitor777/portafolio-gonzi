e6c721d9148535072702b213acaa72a835c35e2c
Lolitor777 <carvajal57685@gmail.com>
2023-11-25 15:33:59 -0500

    Creación del ultimo proyecto y modificación a la card

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const menu = document.querySelector('.nav_menu');
 const menuList = document.querySelector('.nav_list')
 const links = document.querySelectorAll('.nav_link');
+
 const sliders = [...document.querySelectorAll('.slider_body')];
 const arrowAfter = document.getElementById('after');
 const arrowBefore = document.getElementById('before')
+
+const manual = [...document.querySelectorAll('.manual_body')];
+const manualAfterArrow = document.getElementById('after_manual');
+const manualBeforeArrow = document.getElementById('before_manual');
 let value;
+let valueManual;
 
 
 // Funcionalidad del menú hamburguesa
@@ -19,6 +25,28 @@ links.forEach((link)=> {
         menu.classList.remove('nav_menu--active')
     })
 })
+
+
+//Funcionalidad del manual 
+
+manualAfterArrow.addEventListener('click', () => changePositionManual(1));
+
+manualBeforeArrow.addEventListener('click', () => changePositionManual(-1));
+
+const changePositionManual = (changeManual) => {
+    const currentElement = Number(document.querySelector('.manual_body--show').
+    dataset.id);
+
+    valueManual = currentElement;
+    valueManual += changeManual;
+    if (valueManual === 0 || valueManual == manual.length+1) {
+        valueManual = valueManual === 0 ? manual.length : 1;
+    }
+
+    manual[currentElement-1].classList.toggle('manual_body--show');
+    manual[valueManual-1].classList.toggle('manual_body--show');
+}
+
 
 // Funcionalidad del slider
 arrowAfter.addEventListener('click', () => changePosition(1));
